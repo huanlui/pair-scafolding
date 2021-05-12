cb208bfb7400a82149d11d110aa8db224ea6ac37
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2021-05-12 09:16:37 +0200

    Create class UserDetails to collect the user form input

---
+++ src/main/java/org/huanlui/pair/scaffolding/domain/UserDetails.java
@@ -0,0 +1,37 @@
+package org.huanlui.pair.scaffolding.domain;
+
+import java.util.Date;
+import java.util.List;
+
+public class UserDetails {
+
+    private String name;
+
+    private Date dateOfBirth;
+
+    private List<String> hobbies;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+}
