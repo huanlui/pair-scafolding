6dba524a5b54e0c864ae704e4cf79adb07f06d91
Juan Luis García López <juanluis.garcia@thoughtworks.com>
2021-05-12 08:32:01 +0200

    First commit

---
+++ src/test/java/org/huanlui/pair/scaffolding/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.huanlui.pair.scaffolding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
